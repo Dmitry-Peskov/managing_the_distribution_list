187827446b4f4ee4a98075ee00b801c2d0f4dbd3
Dmitry-Peskov <dv.peskov@yandex.ru>
2023-12-12 22:40:48 +0300

    Пофиксил все. Пришлось снести все миграции и накатывать на БД по новой. Пробелма оказалась в BigInteger. В SQLite для него не работает автоинкрементация

--- core/config.py
+++ core/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings
 
 class SettingsDB(BaseSettings):
     url: str = f"sqlite+aiosqlite:///{Path(__file__).parent.parent}/database.sqlite3"
-    echo: bool = True
+    echo: bool = False
 
 
 class SettingsAPI(BaseSettings):
